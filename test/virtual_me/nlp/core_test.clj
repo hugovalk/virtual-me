833083b0190016eaa855e06071c641c75ce1d057
Hugo Valk <hugo.valk@randstadgroep.nl>
2018-03-08 08:50:31 +0100

    Use convenience method to create the texts with the right language.

--- test/virtual_me/nlp/core_test.clj
+++ test/virtual_me/nlp/core_test.clj
@@ -3,8 +3,8 @@
   (:require [virtual-me.nlp.core :refer :all]))
 
 (fact "tokenize splits text in its separate tokens"
-  (:tokens (tokenize {:language :nl :text "Een hele mooie Nederlandse zin."})) => ["Een" "hele" "mooie" "Nederlandse" "zin" "."]
-  (:tokens (tokenize {:language :en :text "A very nice sentence in English."})) => ["A" "very" "nice" "sentence" "in" "English" "."])
+  (:tokens (tokenize (dutch "Een hele mooie Nederlandse zin.") => ["Een" "hele" "mooie" "Nederlandse" "zin" "."]))
+  (:tokens (tokenize (english "A very nice sentence in English.") => ["A" "very" "nice" "sentence" "in" "English" "."])))
 
 (fact "tag-persons can find person names in Dutch sentences"
   (tag-persons (tokenize (dutch "De vriend van Jan Smits heet Jan."))) => ["Jan Smits", "Jan"])
