293aa4920787adae5e78e1ab1e6fdf96efccbc06
Hugo Valk <hugo.valk@gmail.com>
2018-04-29 00:01:51 +0200

    Upgrade project to most recent library versions.

--- project.clj
+++ project.clj
@@ -3,22 +3,22 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.946"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.10.238"]
                  [im.chit/hara.io.scheduler "2.5.10"]
                  [clojure-opennlp "0.4.0"]
                  [org.clojure/tools.logging "0.4.0"]
-                 [ch.qos.logback/logback-classic "1.1.3"]
-                 [compojure "1.6.0"]
-                 [metosin/compojure-api "1.1.11"]
+                 [ch.qos.logback/logback-classic "1.2.3"]
+                 [compojure "1.6.1"]
+                 [metosin/compojure-api "1.1.12"]
                  [ring/ring-defaults "0.3.1"]
-                 [reagent "0.7.0"]
+                 [reagent "0.8.0"]
                  [hiccup "1.0.5"]
-                 [clj-http "3.7.0"]
-                 [cljs-http "0.1.44"]
+                 [clj-http "3.8.0"]
+                 [cljs-http "0.1.45"]
                  [cheshire "5.8.0"]
-                 [garden "1.3.3"]
-                 [figwheel "0.5.14"]]
+                 [garden "1.3.5"]
+                 [figwheel "0.5.15"]]
   :main ^:skip-aot virtual-me.core
   :source-paths ["src/clj", "src/scripts", "src/garden"]
   :resource-paths ["models/nl"
@@ -34,12 +34,12 @@
         :bootclasspath false
         :jvm-opts ["-server" "-Dfile.encoding=utf-8" "$JVM_OPTS"]}
   :profiles {:uberjar {:aot :all}
-             :dev {:plugins [[lein-binplus "0.6.2"]
+             :dev {:plugins [[lein-binplus "0.6.4"]
                              [lein-midje "3.2.1"]
                              [lein-cljsbuild "1.1.7"]
                              [lein-garden "0.3.0"]
-                             [lein-figwheel "0.5.14"]
-                             [lein-ring "0.12.3"]]
+                             [lein-figwheel "0.5.15"]
+                             [lein-ring "0.12.4"]]
                    :dependencies [[midje "1.9.1"]
                                   [ring/ring-mock "0.3.2"]
                                   [javax.servlet/servlet-api "2.5"]]}}
