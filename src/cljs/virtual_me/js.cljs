a7ff8023c95838178070995c49185e111a969801
Hugo Valk <hugo.valk@gmail.com>
2018-04-29 00:02:17 +0200

    Add message to the chat log and clear the input field.

--- src/cljs/virtual_me/js.cljs
+++ src/cljs/virtual_me/js.cljs
@@ -11,6 +11,10 @@
 (defonce time-updater (js/setInterval
                         #(reset! timer (js/Date.)) 1000))
 
+(defn clock []
+  (let [time-str (-> @timer .toTimeString (clojure.string/split " ") first)]
+    [:div.example-clock time-str]))
+
 (defn greeting []
   (let [ping (r/atom "")]
     (r/create-class
@@ -21,17 +25,39 @@
        :reagent-render (fn []
                          [:h2 "you are talking with: " @ping])})))
 
-(defn clock []
-  (let [time-str (-> @timer .toTimeString (clojure.string/split " ") first)]
-    [:div.example-clock time-str]))
+
+(defonce messages (r/atom []))
+
+(defn push-message [message]
+  (reset! messages (conj @messages
+                         {:key (+ 1 (reduce (fn [a,b] (max a (:key b))) 0 @messages))
+                          :text message})))
+
+
+(defn show-messages []
+  (for [message @messages]
+    [:div {:key (:key message)} (:text message)]))
 
 (defn message-input []
-  [:div.message-input
-   [:textarea {:type "text" :rows 3}]])
+  (let [cur-mess (r/atom "")]
+    (fn []
+      [:div.message-input
+       [:textarea {:type "text" :rows 3
+                   :value @cur-mess
+                   :on-change (fn [e]
+                                (reset! cur-mess (-> e .-target .-value)))
+                   :on-key-press (fn [e]
+                                   (when (= (.-key e) "Enter")
+                                     (do
+                                       (.preventDefault e)
+                                       (push-message @cur-mess)
+                                       (reset! cur-mess ""))))}]])))
 
 (defn chat-window []
   [:div.chat-window
    [greeting]
+   [:div.messages-list
+    (show-messages)]
    [message-input]
    [clock]])
 
