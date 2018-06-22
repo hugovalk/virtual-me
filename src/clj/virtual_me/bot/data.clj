e364a7a839ea58cf96b7fe96e3ceb21f35f49de9
Hugo Valk <hugo.valk@gmail.com>
2018-06-22 15:36:24 +0200

    Use parameter instead of value from outer scope.

--- src/clj/virtual_me/bot/data.clj
+++ src/clj/virtual_me/bot/data.clj
@@ -4,20 +4,20 @@
 (defrecord Session [session-id name])
 
 (defprotocol ChatSessionStore
-  (new-session [x name] "Create a new chat session with the name of the user.")
-  (load-session [x session-id] "Load an existing chat session.")
-  (delete-session [x session-id] "Delete a chat session from the store."))
+  (new-session [chat-store name] "Create a new chat session with the name of the user.")
+  (load-session [chat-store session-id] "Load an existing chat session.")
+  (delete-session [chat-store session-id] "Delete a chat session from the store."))
 
 (defrecord InMemoryChatSessionStore [store]
   ChatSessionStore
-  (new-session [x name]
+  (new-session [chat-store name]
     (let [new-session (Session. (.toString (UUID/randomUUID)) name)]
-      (swap! store assoc (keyword (:session-id new-session)) new-session)
+      (swap! (:store chat-store) assoc (keyword (:session-id new-session)) new-session)
       new-session))
-  (load-session [x session-id]
-    ((keyword session-id) @store))
-  (delete-session [x session-id]
-    (swap! store dissoc (keyword session-id))
+  (load-session [chat-store session-id]
+    ((keyword session-id) @(:store chat-store)))
+  (delete-session [chat-store session-id]
+    (swap! (:store chat-store) dissoc (keyword session-id))
     session-id))
 
 (defn init-inmemory-chat-session-store
