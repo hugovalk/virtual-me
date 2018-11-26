6ece2e2a5bb27061b8bc2a431c1bc0bdd7837f86
Hugo Valk <hugo.valk@gmail.com>
2018-11-26 21:53:56 +0100

    Add a word2vec model training script.

--- src/clj/virtual_me/tools.clj
+++ src/clj/virtual_me/tools.clj
@@ -1,13 +1,34 @@
 (ns virtual-me.tools
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io])
   (:import (java.util Scanner)))
+
+(def current-directory-as-string
+  (str/join ""
+            (drop-last 2 (.getAbsolutePath (io/file ".")))))
+
+(defn do-prompt! [prompt]
+  (if (nil? (System/console))
+    (do
+      (print prompt)
+      (flush)
+      (.next (new Scanner (System/in))))
+    (String/valueOf (.readLine (System/console) prompt nil))))
+
 
 (defn swap-atom-via-prompt!
   [value prompt]
-  (swap! value
-         (fn [current-state]
-           (if (nil? (System/console))
-             (do
-               (print prompt)
-               (flush)
-               (.next (new Scanner (System/in))))
-             (String/valueOf (.readLine (System/console) prompt nil))))))
+  (swap! value #{(do-prompt! prompt)}))
+
+(defn change-path!
+  [value description]
+  (let [yn (do-prompt! (str "Do you want to change " (.toLowerCase description) " (" @value ") y/n? "))]
+    (cond
+      (= yn "y") true
+      (= yn "n") false
+      :else (change-path! value description))))
+
+(defn swap-atom-with-path!
+  [value description]
+  (if (change-path! value description)
+      (swap-atom-via-prompt! value (str description ": "))))
--- src/scripts/virtual_me_scripts/nlp/w2v_trainer.clj
+++ src/scripts/virtual_me_scripts/nlp/w2v_trainer.clj
@@ -1,11 +1,50 @@
 (ns virtual-me-scripts.nlp.w2v-trainer
-  (:require [virtual-me.tools :as tools]))
+  (:require [virtual-me.tools :as tools]
+            [clojure.java.io :as io])
+  (:import (org.deeplearning4j.text.sentenceiterator LineSentenceIterator SentencePreProcessor)
+           (org.deeplearning4j.text.tokenization.tokenizerfactory DefaultTokenizerFactory)
+           (org.deeplearning4j.text.tokenization.tokenizer.preprocessor CommonPreprocessor)
+           (org.deeplearning4j.models.word2vec Word2Vec Word2Vec$Builder)
+           (org.deeplearning4j.models.embeddings.loader WordVectorSerializer)
+           (java.io File)))
 
-(def w2v-corpus-path (atom "."))
-(def models-path (atom "."))
+(def w2v-corpus-path (atom (str tools/current-directory-as-string "/dev-resources/w2v/english.txt")))
+(def model-path (atom (str tools/current-directory-as-string "/models/en/w2v")))
+
+(defn sentence-preprocessor []
+  (reify SentencePreProcessor
+    (preProcess [this sentence]
+      (.toLowerCase sentence))))
+
+(defn create-sentence-iterator [path]
+  (let [iter (LineSentenceIterator. (io/file path))]
+    (.setPreProcessor iter (sentence-preprocessor))
+    iter))
+
+(defn create-tokenizer-factory []
+  (let [f (DefaultTokenizerFactory.)]
+    (.setTokenPreProcessor f (CommonPreprocessor.))
+    f))
+
+(defn word2vec [sentence-iterator tokenizer-factory]
+  (let [b (Word2Vec$Builder.)]
+    (.minWordFrequency b 5)
+    (.layerSize b 100)
+    (.seed b 42)
+    (.windowSize b 5)
+    (.epochs b 1)
+    (.iterations b 1)
+    (.iterate b sentence-iterator)
+    (.tokenizerFactory b tokenizer-factory)
+    (.build b)));
 
 (defn -main []
   (do
-    (tools/swap-atom-via-prompt! w2v-corpus-path "corpus text file path:")
-    (tools/swap-atom-via-prompt! models-path "Models directory:")
+    (tools/swap-atom-with-path! w2v-corpus-path "Corpus text file path")
+    (tools/swap-atom-with-path! model-path "Models directory")
+    (.mkdirs (File. @model-path))
+    (let [vec (word2vec (create-sentence-iterator @w2v-corpus-path)
+                        (create-tokenizer-factory))]
+      (.fit vec)
+      (WordVectorSerializer/writeWord2VecModel vec (str @model-path "word-vector-model.txt")))
     (println "done...")))
