41b19de765979f3deaf3dfe6278b9944259544af
Hugo Valk <hugo.valk@gmail.com>
2018-11-27 06:12:07 +0100

    Store TSNE data with the word vector model.

--- src/scripts/virtual_me_scripts/nlp/w2v_trainer.clj
+++ src/scripts/virtual_me_scripts/nlp/w2v_trainer.clj
@@ -1,12 +1,16 @@
 (ns virtual-me-scripts.nlp.w2v-trainer
   (:require [virtual-me.tools :as tools]
+            [virtual-me.dl4j.ui :as ui]
             [clojure.java.io :as io])
   (:import (org.deeplearning4j.text.sentenceiterator LineSentenceIterator SentencePreProcessor)
            (org.deeplearning4j.text.tokenization.tokenizerfactory DefaultTokenizerFactory)
            (org.deeplearning4j.text.tokenization.tokenizer.preprocessor CommonPreprocessor)
            (org.deeplearning4j.models.word2vec Word2Vec Word2Vec$Builder)
            (org.deeplearning4j.models.embeddings.loader WordVectorSerializer)
-           (java.io File)))
+           (java.io File)
+           (org.deeplearning4j.plot BarnesHutTsne BarnesHutTsne$Builder)
+           (java.util ArrayList)
+           (org.nd4j.linalg.factory Nd4j)))
 
 (def w2v-corpus-path (atom (str tools/current-directory-as-string "/dev-resources/w2v/english.txt")))
 (def model-path (atom (str tools/current-directory-as-string "/models/en/w2v")))
@@ -36,15 +40,44 @@
     (.iterations b 1)
     (.iterate b sentence-iterator)
     (.tokenizerFactory b tokenizer-factory)
-    (.build b)));
+    (.build b)))
+
+(defn build-cache-list! [vocab-cache]
+  (let [array (ArrayList.)
+        r (take (.numWords vocab-cache) (range))]
+    (doseq [i r]
+      (.add array (.wordAtIndex vocab-cache i)))
+    array))
+
+(defn tsne-model []
+  (let [b (BarnesHutTsne$Builder.)]
+    (.setMaxIter b 100)
+    (.theta b 0.5)
+    (.normalize b true)
+    (.learningRate b 500)
+    (.useAdaGrad b false)
+    (.build b)))
+
+(defn fit-tsne [vec]
+  (let [tsne (tsne-model)
+        vectors (iterator-seq (.vectors (.lookupTable vec)))]
+    (.fit tsne (reduce
+                 (fn [a b] (Nd4j/vstack (list a b)))
+                 vectors))
+    tsne))
 
 (defn -main []
   (do
+    (ui/start-ui-server)
     (tools/swap-atom-with-path! w2v-corpus-path "Corpus text file path")
     (tools/swap-atom-with-path! model-path "Models directory")
-    (.mkdirs (File. @model-path))
+    (.mkdirs (.getParentFile (File. @model-path)))
     (let [vec (word2vec (create-sentence-iterator @w2v-corpus-path)
                         (create-tokenizer-factory))]
       (.fit vec)
-      (WordVectorSerializer/writeWord2VecModel vec (str @model-path "/word-vector-model.txt")))
+      (WordVectorSerializer/writeWord2VecModel vec (str @model-path "/word-vector-model.txt"))
+      (let [tsne (fit-tsne vec)]
+        (.saveAsFile tsne
+                     (build-cache-list! (.vocab vec))
+                     (str @model-path "/word-vector-tsne.txt"))))
     (println "done...")))
