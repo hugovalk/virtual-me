5332686b3c94b72c8274987713bf6c251cda4b56
Hugo Valk <hugo.valk@gmail.com>
2019-01-04 15:43:56 +0100

    Start defining some intents.

---
+++ src/clj/virtual_me/bot/intents.clj
@@ -0,0 +1,21 @@
+(ns virtual-me.bot.intents
+  (:require [clojure.spec.alpha :as spec]))
+
+(spec/def ::tag string?)
+(spec/def ::pattern (spec/* string?))
+(spec/def ::responses (spec/* string?))
+(spec/def ::intent (spec/keys :req [::tag
+                                    ::pattern
+                                    ::responses]))
+
+(def intents
+  [{::tag "greeting"
+    ::pattern ["hi" "hello" "how are you" "good day" "good morning" "good evening" "good afternoon"]
+    ::responses ["Hi!" "Hello, how are you?" "Good to see you!"]}
+   {::tag "goodbye"
+    ::pattern ["see you later" "goodbye" "good night" "have a nice day"]
+    ::responses ["See you later" "Thanks for visiting" "Have a nice day" "Bye!"]}
+   {::tag "thanks"
+    ::pattern ["thanks" "thank you" "that's helpful"]
+    ::responses ["My pleasure" "Don't mention it" "Any time" "Happy to help"]}])
+
