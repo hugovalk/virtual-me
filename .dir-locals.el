0e0be3219cad32f1e780fdd0432b9e90e9ab154b
Hugo Valk <hugo.valk@gmail.com>
2021-05-19 08:59:57 +0200

    Fix test classpath for Emacs

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,2 @@
-((clojurescript-mode . ((cider-clojure-cli-global-options . "-M:fig"))))
+((clojurescript-mode . ((cider-clojure-cli-global-options . "-M:fig")))
+ (clojure-mode . ((cider-clojure-cli-aliases . "dev-classpath"))))
