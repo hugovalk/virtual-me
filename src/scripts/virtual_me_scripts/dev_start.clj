208f572f75226f479f923cc6429f8396054b8627
Hugo Valk <hugo.valk@gmail.com>
2022-01-27 14:47:13 +0100

    Remove refactor middleware. Does not seem to work.

--- src/scripts/virtual_me_scripts/dev_start.clj
+++ src/scripts/virtual_me_scripts/dev_start.clj
@@ -1,8 +1,7 @@
 (ns virtual-me-scripts.dev-start
   (:require [virtual-me.httpkit :refer [-main]]
             [nrepl.server :refer [start-server]]
-            [cider.nrepl :refer [cider-nrepl-handler]]
-            [refactor-nrepl]))
+            [cider.nrepl :refer [cider-nrepl-handler]]))
 
 (defonce server (start-server :port 5555 :handler cider-nrepl-handler))
 (-main)
